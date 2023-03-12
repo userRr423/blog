2a3a4a180cd64944dfc0ac5d0c8f5a3212fb7040
ruslanhristoforoff@yandex.ru <ruslanhristoforoff@yandex.ru>
2023-03-12 15:02:29 +0300

    php

--- pages/articles.php
+++ pages/articles.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, width=device-width">
-    <title>Главная страница</title>
+    <title>Все статьи</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
